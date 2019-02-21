b8ae1af41990fcdce21e0d97f95dc91b157cf761
Tomasz Krakowiak <tomasz.krakowiak@ericsson.com>
2019-02-21 17:59:22 +0100

    Order validation with tests

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -3,10 +3,13 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.Order;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -15,12 +18,15 @@ public class OrderController {
 
     @GetMapping("/current")
     public String orderForm(Model model) {
-        model.addAttribute("order", new Order());
+        model.addAttribute("order", Order.builder().build());
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(Order order) {
+    public String processOrder(@Valid Order order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
--- src/test/groovy/tacos/web/OrderControllerSpec.groovy
+++ src/test/groovy/tacos/web/OrderControllerSpec.groovy
@@ -3,7 +3,10 @@ package tacos.web
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import static org.hamcrest.Matchers.containsString
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -27,9 +30,56 @@ class OrderControllerSpec extends Specification {
 
     def "should process post request an redirect to home"() {
         expect:
-        mockMvc.perform(post("/orders"))
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>()
+
+        mockMvc.perform(post("/orders")
+                .param("name", "Tom")
+                .param("street", "Polczeki")
+                .param("city", "Warsaw")
+                .param("state", "Mazowieckie")
+                .param("zip", "03-984")
+                .param("ccNumber", "4532023303729686")
+                .param("ccExpiration", "12/39")
+                .param("ccCVV", "123"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/"))
+    }
+
+    @Unroll
+    def "should process post request, stay on orderForm page and report missing #fields"() {
+        given:
+        MultiValueMap<String, String> form = new LinkedMultiValueMap<String, String>()
+
+        expect:
+        mockMvc.perform(post("/orders")
+                .params(inputForm))
+                .andExpect(model().attributeDoesNotExist(inputForm.findAll { !it.value }.collect {
+            it.key
+        } as String[]))
+                .andExpect(status().is2xxSuccessful())
+
+        where:
+        fields                                         | inputForm
+        "name"                                         | [name: [""], street: ["Grochowska"], city: ["Warsaw"], state: ["Mazowieckie"], zip: ["03-984"], ccNumber: ["4532023303729686"], ccExpiration: ["12/35"], ccCVV: ["123"]] as LinkedMultiValueMap
+        "street,state,zip,ccNumber,ccExpiration,ccCVV" | [name: ["Tom"], street: [""], city: [""], state: [""], zip: [""], ccNumber: [""], ccExpiration: [""], ccCVV: [""]] as LinkedMultiValueMap
+    }
+
+    @Unroll
+    def "should process post request stay on orderForm page and report errors for #testDesc"() {
+        given:
+        MultiValueMap<String, String> form = new LinkedMultiValueMap<String, String>()
+
+        expect:
+        mockMvc.perform(post("/orders")
+                .params(inputForm))
+                .andExpect(model().attributeHasFieldErrorCode("order", fieldName, error))
+                .andExpect(status().is2xxSuccessful())
+
+        where:
+        testDesc                       | inputForm                                                                                                                                                                                            || fieldName      | error
+        "incorrect Credit Card number" | [name: ["Tom"], street: ["Grochowska"], city: ["Warsaw"], state: ["Mazowieckie"], zip: ["03-984"], ccNumber: ["1234"], ccExpiration: ["12/35"], ccCVV: ["123"]] as LinkedMultiValueMap               || "ccNumber"     | "CreditCardNumber"
+        "incorrect Expiration Date"    | [name: ["Tom"], street: ["Grochowska"], city: ["Warsaw"], state: ["Mazowieckie"], zip: ["03-984"], ccNumber: ["4532023303729686"], ccExpiration: ["12/2035"], ccCVV: ["123"]] as LinkedMultiValueMap || "ccExpiration" | "Pattern"
+        "incorrect Credit Card CVV"    | [name: ["Tom"], street: ["Grochowska"], city: ["Warsaw"], state: ["Mazowieckie"], zip: ["03-984"], ccNumber: ["4532023303729686"], ccExpiration: ["12/35"], ccCVV: ["12345"]] as LinkedMultiValueMap || "ccCVV"        | "Digits"
     }
 
 }
